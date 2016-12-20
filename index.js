8fb4614f1d871907eb99c07d5bda9c2fe6742d0d
Ryan Lutgen <ryanlutgen@outlook.com>
2016-12-20 00:53:00 -0600

    extract only the sheets we are about, not hidden ones.  Add processing for third party commercial sets

--- index.js
+++ index.js
@@ -6,14 +6,27 @@ let _ = require('lodash');
 let mainSet = require('./parsers/main-set');
 let expansions = require('./parsers/expansions');
 let packs = require('./parsers/packs');
+let thirdPartyCommercial = require('./parsers/third-party-commercial');
 
 const worksheet = xlsx.parse(`${__dirname}/data/cah-cards.xlsx`);
 
+var sheetsToScan = ["CAH Main Deck", "CAH Expansions", "CAH Packs", "Third Party Commercial", "Kickstarter", "Stand Alone Games", "Print On Demand",
+    "Special Purpose", "Fan Expansions", "Misc. Unofficial Decks - Black", "Misc. Unofficial Decks - White", "Custom - White", "Custom - Black", "More Custom Cards"];
+
+let sheets = [];
+_.each(worksheet, (sheet) => {
+    var index = _.findIndex(sheetsToScan, (sheetScanned) => { return sheetScanned === sheet.name; });
+    if (index !== -1) {
+        sheets.push(sheet);
+    }
+});
+
 let cards = [];
 //keep cards in seperate arrays for now to debug
-cards.push(mainSet.parse(worksheet[3]).cards);
-cards.push(expansions.parse(worksheet[4]).cards);
-cards.push(packs.parse(worksheet[5]).cards);
+cards.push(mainSet.parse(sheets[0]).cards);
+cards.push(expansions.parse(sheets[1]).cards);
+cards.push(packs.parse(sheets[2]).cards);
+cards.push(thirdPartyCommercial.parse(sheets[3]).cards);
 
 
 fs.writeFileSync('./output/test.json', JSON.stringify(cards, null, 4));
---
+++ parsers/third-party-commercial.js
@@ -0,0 +1,100 @@
+"use strict";
+
+let _ = require('lodash');
+
+const HEADERS = {
+    "card_type": 0,
+    "card_name": 1,
+    "special": 2
+};
+
+const CARD_TEMPLATE = {
+    "card_type": "",
+    "card_name": "",
+    "card_special": "",
+    "card_set": ""
+};
+
+/**
+ * returns an object of coordinate locations and names of each pack
+ * @param sheet
+ * @returns {{}}
+ */
+function findSetNamesAndCoords(sheet) {
+    let coordTemplate = {"x": 0, "y": 0, "name": ""};
+    let coords = [];
+
+    _.each(sheet.data, (row, rowIndex) => {
+        _.each(row, (item, itemIndex, itemArray) => {
+            if (itemIndex !== 0 && item !== undefined) {
+                if (itemArray[itemIndex - 1] === "Set" && itemArray[itemIndex + 1] === "Special") {
+                    console.log("x: " + (rowIndex + 2) + ", y = " + (itemIndex + 1) + " name: " + item);
+                    let cahSet = _.clone(coordTemplate);
+                    cahSet["x"] = rowIndex;
+                    cahSet["y"] = itemIndex;
+                    cahSet["name"] = item;
+                    coords.push(cahSet);
+                }
+            }
+        });
+    });
+
+    return coords;
+}
+
+function findLastCardSetX(coord, sheet) {
+    let y = coord.y - 1;
+
+    let firstResponseX = 0;
+    let lastResponseX = 0;
+    for (let i = coord.x; i < sheet.data.length; i++) {
+        let row = sheet.data[i];
+
+        if (row[0] === 'Response' && firstResponseX === 0) {
+            firstResponseX = i;
+        }
+
+        if (row[0] === undefined && firstResponseX !== 0) {
+            lastResponseX = i - 1;
+            break;
+        }
+    }
+
+    return {"x": lastResponseX, "y": y}
+}
+
+module.exports = {
+    parse: function(sheet) {
+        let cards = [];
+        let coords = findSetNamesAndCoords(sheet);
+
+        console.log(coords);
+
+        //similar to official packs, latch onto the set name, then find the last response row.  Iterate from
+        //the set name to the last response row
+        _.each(coords, (coord) => {
+            let x = coord["x"], y = coord["y"], name = coord["name"];
+            let cardTypeCoordY =  y - 1;
+            let cardSet = [];
+            let lastResponseCoord = findLastCardSetX(coord, sheet);
+
+            for (let i = x; i <= lastResponseCoord.x; i++) {
+                let row = sheet.data[i];
+
+                let cardType = row[cardTypeCoordY];
+                if (cardType === "Prompt" || cardType === "Response") {
+                    let card = _.clone(CARD_TEMPLATE);
+                    card.card_type = cardType;
+                    card.card_name = row[cardTypeCoordY + HEADERS["card_name"]];
+                    card.card_special = row[cardTypeCoordY + HEADERS["card_special"]];
+                    card.card_set = coord["name"];
+                    cardSet.push(card);
+                }
+            }
+
+            cards.push(cardSet);
+        });
+
+        return {"set": sheet.name, "cards": cards};
+    }
+};
